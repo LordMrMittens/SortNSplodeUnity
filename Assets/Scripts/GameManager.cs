ac8d5596e66553334d0e4934da038da80f41ac5f
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-08-11 11:09:20 +0100

    Scoring Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager Instance;
-    public int score;
+    public int score=0;
+    [SerializeField] TMP_Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+        UpdateScoreText();
     }
 
     public void AddToScore(int value){
         score += value;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText(){
+        scoreText.text = score.ToString();
     }
 }
