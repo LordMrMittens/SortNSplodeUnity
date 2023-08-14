11045031c0dc2793aad6dc59fa04250cda085e21
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-08-14 10:03:45 +0100

    Teleport bug

--- Assets/Scripts/DepositBox.cs
+++ Assets/Scripts/DepositBox.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class DepositBox : MonoBehaviour
@@ -10,75 +11,96 @@ public class DepositBox : MonoBehaviour
     private Vector3 destination;
     private Vector3 startingPoint;
     public float distanceToTravel;
+    public Vector3 direction;
     private float startTime;
     public float movementSpeed = 2.0f;
-    public Vector3 direction;
 
     bool shouldMove = false;
     bool shouldReturn = false;
-    
-    private void Start() {
+
+    private void Start()
+    {
         startingPoint = transform.position;
-        destination = ((direction - startingPoint).normalized) * distanceToTravel;
+        direction = // Define your direction here.
+        destination = startingPoint + direction.normalized * distanceToTravel;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(containedHamsters.Count ==numberContainedToScore){
-            StartCoroutine(MoveBoxCoroutine(startingPoint, destination));
+        if (containedHamsters.Count == numberContainedToScore && !shouldMove)
+        {
+            shouldMove = true;
+            startTime = Time.time;
             
+            StartCoroutine(MoveBoxCoroutine(startingPoint, destination));
+        }
+        if(shouldReturn && !shouldMove){
+            shouldMove = true;
+            StartCoroutine(MoveBoxCoroutine(destination, startingPoint));
+            Debug.Log("returning");
         }
     }
-    private void OnTriggerStay(Collider other) {
-        if(other.CompareTag("Hamster")){
 
-            containedHamsters.Add(other.gameObject.GetComponent<Hamster>());
-            other.tag="ContainedHamster";
-        }
-    }
-
-    IEnumerator MoveBoxCoroutine(Vector3 _startingPoint, Vector3 _destination)
+    private void OnTriggerStay(Collider other)
     {
+        if (other.CompareTag("Hamster"))
+        {
+            containedHamsters.Add(other.gameObject.GetComponent<Hamster>());
+            other.tag = "ContainedHamster";
+        }
+    }
 
-            float journeyLength = Vector3.Distance(_startingPoint, _destination);
+    IEnumerator MoveBoxCoroutine(Vector3 _startingPoint, Vector3 _Destination)
+    {
+        while (shouldMove)
+        {
+            float journeyLength = Vector3.Distance(_startingPoint, _Destination);
             float distanceCovered = (Time.time - startTime) * movementSpeed;
 
             if (distanceCovered < journeyLength)
             {
                 float fractionOfJourney = distanceCovered / journeyLength;
-                transform.position = Vector3.Lerp(_startingPoint, _destination, fractionOfJourney);
+                transform.position = Vector3.Lerp(_startingPoint, _Destination, fractionOfJourney);
             }
             else
             {
-                transform.position = _destination;
-                if (_destination != startingPoint)
-                {
-                    shouldReturn = true;
-                    StartCoroutine(AddScores());
-                }
-                else
-                {
-                    shouldReturn = false;
-                }
+                transform.position = _Destination;
+                shouldMove = false;
             }
-            yield return new WaitUntil(() => shouldReturn == true);
+            
+            yield return null;
+        }
+        Debug.Log("logging this");
+        if (shouldReturn == false)
+        {
+            
+            StartCoroutine(AddScores());
+            Debug.Log("attempting this");
+
+        } else {
+            shouldMove=false;
+            shouldReturn=false;
+        }
         
     }
 
     IEnumerator AddScores()
     {
-
         GameManager.Instance.AddToScore(containedHamsters.Count);
-        for (int i = 0; i < containedHamsters.Count; i++)
+        
+        foreach (Hamster hamster in containedHamsters)
         {
-            Destroy(containedHamsters[i].gameObject);
+            Destroy(hamster.gameObject);
         }
         containedHamsters.Clear();
-        
-        yield return new WaitUntil(() => containedHamsters.Count <= 0);
-        shouldMove= true;
-        StartCoroutine(MoveBoxCoroutine(destination, startingPoint));
+        startTime = Time.time;
+        shouldReturn = true;
+        Debug.Log("finished Scores");
+        yield return null;
     }
-
 }
+
+
+
+
+
